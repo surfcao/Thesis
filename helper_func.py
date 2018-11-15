9de6f6b3fd94d6945b725f14342319307cd09096
Sidhanth Krishna <43849409+sidhanth97@users.noreply.github.com>
2018-11-15 14:59:09 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -27,16 +27,17 @@ def load_batch(path,init_shape,scale=2,end=25000):
 
 	
 
-def test_network(img_path,prediction,x,y,train,init_shape=128,scale=2):
+def test_network(img_path,prediction,x,y,train,lr,init_shape=128,scale=2):
 	img = cv2.imread(img_path,1)	
 	img_x,img_y = cv2.resize(img,(init_shape,init_shape),interpolation=cv2.INTER_CUBIC),cv2.resize(img,(init_shape*scale,init_shape*scale),interpolation=cv2.INTER_CUBIC)
 	img_x,img_y = np.expand_dims(img_x,axis=0),np.expand_dims(img_y,axis=0)
-	feed = {train:False,x:img_x,y:img_y}	
+	img_x,img_y = cv2.cvtColor(img_x,cv2.COLOR_BGR2RGB),cv2.cvtColor(img_y,cv2.COLOR_BGR2RGB)
+	feed = {train:False,x:img_x,y:img_y,lr:0.001}	
 	img = prediction.eval(feed_dict=feed).astype('uint8')
 	img = np.reshape(img,(init_shape*scale,init_shape*scale,3))
-	image = y.eval(feed_dict=feed).astype('uint8')
-	image = np.reshape(image,(init_shape*scale,init_shape*scale,3))
-	cv2.imshow('true_image',image)
-	cv2.imshow('generated_image',img)
+	img_x,img_y,img = cv2.cvtColor(img_x,cv2.COLOR_RGB2BGR),cv2.cvtColor(img_y,cv2.COLOR_RGB2BGR),cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+	cv2.imshow('lr_image',img_x)
+	cv2.imshow('hr_image',img_y)
+	cv2.imshow('generated_hr_image',img)
 	cv2.waitKey(0)
 
